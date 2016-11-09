16c3280f852463e27cac04c19ee30eccea520eba
green-ghost <gg-cmh-gs@yandex.ru>
2016-11-09 10:46:06 +0400

    Verson  0.2b * Cahnged xhr to synchronous

--- src/process.js
+++ src/process.js
@@ -1,5 +1,5 @@
 "use strict"
-var players=[],result={},
+var players=[],result={},MAX_THREADS=5,
   rwct='<div id=main_dialog class="ui-outputpanel ui-widget GS_output"><div class="ui-dialog ui-widget ui-widget-content ui-corner-all ui-shadow ui-draggable ui-overlay-visible" style="width: auto; height: auto; left: 713px; top: 15.5px; visibility: visible; z-index: 1005;" role=dialog aria-labelledby=dialog0_title aria-hidden=false aria-live=polite><div class="ui-dialog-titlebar ui-widget-header ui-helper-clearfix ui-corner-top ui-draggable-handle"><span id=dialog0_title class=ui-dialog-title>Результаты обработки</span><a href=# class="ui-dialog-titlebar-icon ui-dialog-titlebar-close ui-corner-all" role=button><span class="ui-icon ui-icon-closethick"/></a></div><div class="ui-dialog-content ui-widget-content" style="height: auto;"><table><thead><tr><td>имя</td><td colspan=6>каналы</td></thead><tbody class=process_result_container></tbody></table></div><div class="ui-dialog-footer ui-widget-content"></div></div></div>',
   //rwcs='{{css_template}}',
   rwlt='<tr><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>',
@@ -14,18 +14,32 @@ $("td[role=gridcell] span").each(function(i,v) {if(v.title.length)players.push(v
 //REMOVE THIS!!!!
 //players=["Ackol0","Aleks222"];
 
+
 $.each(players, collectData);
 
+//function sleep(ms) {return new Promise(function(resolve){setTimeout(resolve, ms)})}
+
+//var gi=-1; //global counter
+//var vs;
+//var interval=setInterval(function(){
+	//var vst=$("#search_tab\\:player_search\\:search_form [name=javax\\.faces\\.ViewState]").val();
+	//if (vs==vst) return; //wait for ViewState updated;
+	//vs=vst;
+//	if(!players[++gi]) clearInterval(interval)
+//	else{collectData(players[gi])}
+//},10)
 
 function collectData(_,name){ //for script size optimization
+
 if (!name.length || typeof(name)!="string") return; //do nothing if name is empty or not a string
-console.log("processing: "+name);
 $.ajax({
-  url:"/GIMPLI-ChatModWebsite/secure/main.faces",
+  url:"http://chatmod.de.heroes-online.com/GIMPLI-ChatModWebsite/secure/main.faces",
   method: "post",
-  headers: {
+  "async":false,
+  headers: {	
 	"Accept": "application/xml, text/xml, */*; q=0.01", 
-	"Faces-Request": "partial/ajax"
+	"Faces-Request": "partial/ajax",
+	"Expires" : 0
   },
   data:{
     "javax.faces.partial.ajax": true,
@@ -53,7 +67,6 @@ $.ajax({
 		.nodeValue
 	
 	  var jqr=$(rhtml);
-	  console.log(jqr.find("#search_tab\\:player_search\\:search_form\\:online").html());
 	  if (jqr.find("#search_tab\\:player_search\\:search_form\\:online").html()=="false") {result[name]={"online_status":false}; return;} //if user offline
 	  result[name]={"online_status":true, "channels":[]} //init current user obj;
 	  var channels_array;
@@ -63,13 +76,15 @@ $.ajax({
 	  if (!channels_array.length) return; //stop processsing if channels array is empty
 	  var rwl=$(rwlt).appendTo(rwc); //insert new line to result contatiner
 	  rwl.find("td:eq(0)").html(name);
-	  $(channels_array).each(function(i,v) {
+	  //$(channels_array).each(function(i,v) {
+	  for(var i in channels_array) {var v=channels_array[i];
 		  v=v.trim();
 		  if(!v.length) return; //stop processing if value is empty
 		  var ar=v.split('_'),pos=(new Number(ar[0]))+1;
 		  result[name].channels.push(v); //push to result var
 		  rwl.find("td:eq("+pos+")").html(v); //and set table cell content
-      })
+	  }
+      //})
   },
   error: function(){}
 })
